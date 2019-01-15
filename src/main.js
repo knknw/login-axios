f371331a7b3ab993733cba4bdef327ef482c98a9
knknw <925128914@qq.com>
2019-01-15 23:49:42 +0800

    dneglu

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import router from './router/touter'
 
 Vue.config.productionTip = false
 
+//引入element-UI组件
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+//注册element-UI组件
+Vue.use(ElementUI)
+import axios from "axios"
+//配置全局请求头
+axios.defaults.baseURL = 'http://litc.pro:9999/v1/'
+//将axios挂到vue的原型上,这样就可以全局访问axios,不必每个页面都重复引入了
+Vue.prototype.$axios = axios
 /* eslint-disable no-new */
 new Vue({
   el: '#app', //el的作用是 将vue实例与主业html页面像关联
--- src/parges/login.vue
+++ src/parges/login.vue
@@ -1,5 +1,78 @@
 <template>
-    <div>
-        <h2>login</h2>
+  <div class="login-comt">
+    <div class="lihon-dv">
+      <h3 class="title">请登录</h3>
+      <el-form :label-position="labelPosition" label-width="80px" >
+        <el-form-item label="用户名">
+          <el-input v-model="username"></el-input>
+        </el-form-item>
+        <el-form-item label="密码">
+          <el-input v-model="parsswrod"></el-input>
+        </el-form-item>
+      </el-form>
+      <el-button type="primary" class="dla" @click="login">登录</el-button>
     </div>
+  </div>
 </template>
+<script>
+//import $axios from 'axios'
+export default {
+    
+  data() {
+    return {
+      labelPosition: "right",
+        parsswrod:"",
+        username:"",
+    }
+  },
+  methods:{
+      login(){
+        
+          //$axios.post('/users/login',{
+            this.$axios.post('users/login',{
+              parsswrod:this.parsswrod,
+              username : this.username
+          }).then(result => {
+                console.log(124)
+              console.log(result.data)
+          }).catch(err => {
+              console.dir(err)
+          })
+      }
+  }
+};
+</script>
+<style lang="less" >
+.login-comt {
+  .lihon-dv {
+    width: 900px;
+    height: 400px;
+    border: 1px #aaa solid;
+    border-radius: 10px;
+    margin: 100px auto;
+    padding-top: 80px;
+    padding-left:180px;
+    .el-input__inner {
+      width: 70%;
+      height: 50px;
+    }
+    .el-form-item__label {
+      font-size: 16px;
+      font-weight: bold;
+    }
+    .title {
+         width: 70%;
+      font-size: 24px;
+      font-weight: bold;
+      padding-bottom: 15px;
+      text-align: center;
+    }
+    .dla{
+        margin-left: 260px;
+        width: 150px;
+        height: 60px;
+        font-size: 25px;
+    }
+  }
+}
+</style>
